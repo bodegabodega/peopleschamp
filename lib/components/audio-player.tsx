537cdbc2e647e883e0fe07a1c24d64f848d752c8
Nikolai Pohorelsky <nikolai.pohorelsky@showtime.net>
2026-01-11 14:36:47 -0500

    1. Nearly there from a styling perspective

--- lib/components/audio-player.tsx
+++ lib/components/audio-player.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import AudioPlayer from 'react-h5-audio-player';
+import Image from "next/image"
+import AudioPlayer, { RHAP_UI } from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+import './audio-player.css';
+
+import playIcon from './../../public/play.svg'
 
 import { useEffect, useRef } from "react";
 import { useAudioPlayerStore } from "@/lib/stores/audio-player-store";
 import { clsx } from 'clsx';
+import Marquee from "react-fast-marquee";
 
 export default function AudioPlayerComponent() {
   const currentTrack: AudioTrack | null = useAudioPlayerStore((state) => state.currentTrack);
@@ -17,12 +22,36 @@ export default function AudioPlayerComponent() {
         "fixed bottom-0 left-0 w-full z-50",
         "bg-white shadow-lg",
         "transform transition-transform duration-300 ease-out",
-        isPlaying ? "translate-y-0" : "translate-y-full"
+        // isPlaying ? "translate-y-0" : "translate-y-full"
+        true ? "translate-y-0" : "translate-y-full"
       )}>
       <AudioPlayer
         onPlay={e => console.log("onPlay")}
         src={currentTrack ? currentTrack.url : undefined}
         autoPlayAfterSrcChange={true}
+        layout='horizontal-reverse'
+        showJumpControls={false}
+        showSkipControls={false}
+        customControlsSection={[RHAP_UI.MAIN_CONTROLS, RHAP_UI.ADDITIONAL_CONTROLS]}
+        // customControlsSection={[RHAP_UI.MAIN_CONTROLS]}
+        customAdditionalControls={[
+          <Marquee>{currentTrack?.name}</Marquee>
+        ]}
+        customProgressBarSection={[RHAP_UI.CURRENT_TIME, RHAP_UI.PROGRESS_BAR, RHAP_UI.DURATION]}
+        customIcons={{
+          play: <Image
+            src="/play.svg"
+            alt="Play Icon"
+            width={20}
+            height={20}
+          />,
+          pause: <Image
+            src="/pause.svg"
+            alt="Pause Icon"
+            width={20}
+            height={20}
+          />
+        }}
         // other props here
       />
     </footer>
